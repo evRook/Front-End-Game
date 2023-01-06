4f200073837bd7b35eda3110045ea76cdddfc813
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-05 17:35:09 -0700

    button listeners

--- java.js
+++ java.js
@@ -1,1 +1,23 @@
 console.log('hello world')
+
+let redBtn = document.querySelector('.red--btn')
+let blueBtn = document.querySelector('.blue--btn')
+let greenBtn = document.querySelector('.green--btn')
+let yellowBtn = document.querySelector('.yellow--btn')
+let storedColors = []
+
+redBtn.addEventListener('click', redTest => {
+    console.log('red')
+})
+
+blueBtn.addEventListener('click', blueTest => {
+    console.log('blue')
+})
+
+greenBtn.addEventListener('click', greenTest => {
+    console.log('green')
+})
+
+yellowBtn.addEventListener('click', yellowTest => {
+    console.log('yellow')
+})
