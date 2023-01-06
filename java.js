87248c44e185793eef00c8268b1a47beb157481a
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-06 08:08:31 -0700

    rethinking

--- java.js
+++ java.js
@@ -4,45 +4,126 @@ let soloButton = document.querySelectorAll('.game--btn')
 let compChoices = ['red','blue','green','goldenrod']
 let compStoredColors = []
 let playerStoredColors = []
+let tempStorage = []
 
-buttons.addEventListener('click', userInput = (evt) => {
-    playerStoredColors.push(evt.target.getAttribute("data-color"))
-})
 
-// function cpuChooses() {
-//     for(i=0; i<compChoices.length; i++){
-//         console.log(compChoices[i])
+
+
+
+
+
+
+
+
+
+
+//tabling this for now
+
+// buttons.addEventListener('click', userInput = (evt) => {
+//     playerStoredColors.push(evt.target.getAttribute("data-color"))
+//     console.log(playerStoredColors)
+// })
+
+// // function cpuChooses() {
+// //     for(i=0; i<compChoices.length; i++){
+// //         console.log(compChoices[i])
+// //     }
+// // }
+// // cpuChooses()
+
+// function cpuChooses(){
+//     let randomColor = Math.floor(Math.random() * compChoices.length)
+//     compStoredColors.push(compChoices[randomColor])
+// }
+
+// function compTurn() {
+
+//     cpuChooses();
+//     console.log(compStoredColors)
+
+// }
+
+
+// function playerTurn() {
+
+//     //clear playerStoredColor
+
+// }
+
+
+
+//tabling this for now
+
+// compStoredColors.forEach(()=>{
+//     for(i=0; i<soloButton.length; i++){
+        
+//     }
+// })
+
+
+// function compTurn(){
+
+//     cpuChooses();
+
+
+
+    // for(i=0; i < compStoredColors.length; i++){
+    //     for(j=0; j<soloButton.length; j++) {
+    //         let litButton = soloButton[j].getAttribute("data-color")
+    //         let litColor = compStoredColors[i]
+    //         let originalColor = soloButton[j].getAttribute("data-original")
+    //         let originalButton = soloButton[j]
+    //         console.log(litColor)
+    //         if(litButton === litColor){
+    //             soloButton[j].style.backgroundColor = `${litColor}`
+    //             setTimeout(()=> {
+    //                 originalButton.style.backgroundColor = `${originalColor}`
+    //             }, 1100 - (i * 20));
+    //         }
+    //     }
+    // } 
+
+    
+// compStoredColors.forEach(() => {
+//     console.log(compStoredColors)
+// })
+
+// soloButton.forEach(() => {
+//     console.log(soloButton)
+// })
+
+
+
+    // let litColor;
+    // let originalButton;
+
+    // for(i=0; i<compStoredColors.length; i++){
+    //     litColor = compStoredColors[i]
+    //     console.log(litColor)
+    // }
+
+    // for(j=0; j<soloButton.length; j++){
+    //     originalButton = soloButton[j]
+    //     console.log(originalButton)
+    // }
+
+    // const match = compStoredColors.filter(element => compChoices.includes(element))
+    // console.log(match)
+    
+
+// }
+// compTurn()
+
+
+
+// let test(a,b){
+//     for (let i = 0; i<a.length;i++){
+//         for(let k = 0; k<b.length: k++){
+//             if(a[i] === b[k]){
+
+//             }
+//         }
 //     }
 // }
-// cpuChooses()
 
-function cpuChooses(){
-    let randomColor = Math.floor(Math.random() * compChoices.length)
-    compStoredColors.push(compChoices[randomColor])
-}
-
-
-
-function compTurn(){
-
-    cpuChooses();
-
-    for(i=0; i < compStoredColors.length; i++){
-        for(j=0; j<soloButton.length; j++) {
-            let litButton = soloButton[j].getAttribute("data-color")
-            let litColor = compStoredColors[i]
-            let originalColor = soloButton[j].getAttribute("data-original")
-            let originalButton = soloButton[j]
-            if(litButton === litColor){
-                soloButton[j].style.backgroundColor = `${litColor}`
-                function flashButton(){
-                    originalButton.style.backgroundColor = `${originalColor}`
-                }
-                setTimeout(flashButton, 1100 - (i*20));
-            }   
-        }
-    }
-
-
-}
-compTurn()
+// test(compStoredColors,soloButton)
