8e06405c6f95f674a610b9fed31372b2cd79a972
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-01-05 19:07:38 -0700

    start of comp turn function & start of loops to light up buttons

--- java.js
+++ java.js
@@ -4,16 +4,14 @@ let redBtn = document.querySelector('.red--btn') //gets red button
 let blueBtn = document.querySelector('.blue--btn') //gets blue button
 let greenBtn = document.querySelector('.green--btn') //gets green button
 let yellowBtn = document.querySelector('.yellow--btn') //gets yellow button
-let buttons = document.querySelector('.game--btns')
+let buttons = document.querySelector('.game--btn__container')
+let soloButton = document.querySelectorAll('.game--btn')
 let compChoices = ['red','blue','green','yellow']
 let compStoredColors = []
 let playerStoredColors = []
 
 buttons.addEventListener('click', userInput = (evt) => {
     playerStoredColors.push(evt.target.getAttribute("data-color"))
-    console.log(playerStoredColors)
-    cpuChooses();
-    console.log(compStoredColors)
 })
 
 // function cpuChooses() {
@@ -27,3 +25,18 @@ function cpuChooses(){
     let randomColor = Math.floor(Math.random() * compChoices.length)
     compStoredColors.push(compChoices[randomColor])
 }
+
+function compTurn(){
+
+    cpuChooses();
+
+    for(i=0; i < compStoredColors.length; i++){
+        for(j=0; j<soloButton.length; j++) {
+            let litButton = soloButton[j].getAttribute("data-color")
+            console.log(litButton)
+        }
+    }
+
+
+}
+compTurn()
